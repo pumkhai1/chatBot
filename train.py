2431dbc242444050dbab06adad1f7381b9ee4d8d
Pum Khai <pumk2000@gmail.com>
2022-08-13 23:55:38 -0400

    new changes

--- train.py
+++ train.py
@@ -11,98 +11,107 @@ from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 from ChatDataset import ChatDataset
 
+
 # all def and functions
-def open_file(name):
+def open_file (name):
     with open (name, 'r') as f:
         intents = json.load (f)
     return intents
 
-# all other code
-intents = open_file('intents.json')
 
-all_words = []
-tags = []
-xy = []
+def append_all_words_tags_and_xy (intents):
+    all_words = [ ]
+    tags = [ ]
+    xy = [ ]
+
+    # loop through each sentence in our intents patterns
+    for intent in intents [ 'intents' ]:
+        tag = intent [ 'tag' ]
+        # add to tag list
+        tags.append (tag)
+        for pattern in intent [ 'patterns' ]:
+            # tokenize each word in the sentence
+            w = tokenize (pattern)
+            # add to our words list
+            all_words.extend (w)
+            # add to xy pair
+            xy.append ((w, tag))
+    return all_words, tags, xy
+
+
+# all other code
+intents = open_file ('intents.json')
 # loop through each sentence in our intents patterns
-for intent in intents['intents']:
-    tag = intent['tag']
-    # add to tag list
-    tags.append(tag)
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = tokenize(pattern)
-        # add to our words list
-        all_words.extend(w)
-        # add to xy pair
-        xy.append((w, tag))
+
+all_words, tags, xy = append_all_words_tags_and_xy(intents)
 
 # stem and lower each word
-ignore_words = ['?', '.', '!']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
+ignore_words = [ '?', '.', '!' ]
+all_words = [ stem (w) for w in all_words if w not in ignore_words ]
 # remove duplicates and sort
-all_words = sorted(set(all_words))
-tags = sorted(set(tags))
+all_words = sorted (set (all_words))
+tags = sorted (set (tags))
 
 # create training data
-X_train = []
-y_train = []
+X_train = [ ]
+y_train = [ ]
 for (pattern_sentence, tag) in xy:
     # X: bag of words for each pattern_sentence
-    bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
+    bag = bag_of_words (pattern_sentence, all_words)
+    X_train.append (bag)
     # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
-    label = tags.index(tag)
-    y_train.append(label)
+    label = tags.index (tag)
+    y_train.append (label)
 
-X_train = np.array(X_train)
-y_train = np.array(y_train)
+X_train = np.array (X_train)
+y_train = np.array (y_train)
 
 # Hyper-parameters
 num_epochs = 1000
 batch_size = 8
 learning_rate = 0.001
-input_size = len(X_train[0])
+input_size = len (X_train [ 0 ])
 hidden_size = 8
-output_size = len(tags)
-print(input_size, output_size)
+output_size = len (tags)
+print (input_size, output_size)
 
-dataset = ChatDataset(X_train, y_train)
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+dataset = ChatDataset (X_train, y_train)
+train_loader = DataLoader (dataset=dataset,
+                           batch_size=batch_size,
+                           shuffle=True,
+                           num_workers=0)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
+device = torch.device ('cuda' if torch.cuda.is_available () else 'cpu')
+model = NeuralNet (input_size, hidden_size, output_size).to (device)
 
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss ()
+optimizer = torch.optim.Adam (model.parameters (), lr=learning_rate)
 
 # Train the model
-for epoch in range(num_epochs):
+for epoch in range (num_epochs):
     for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(dtype=torch.long).to(device)
+        words = words.to (device)
+        labels = labels.to (dtype=torch.long).to (device)
 
         # Forward pass
-        outputs = model(words)
+        outputs = model (words)
         # if y would be one-hot, we must apply
         # labels = torch.max(labels, 1)[1]
-        loss = criterion(outputs, labels)
+        loss = criterion (outputs, labels)
 
         # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        optimizer.zero_grad ()
+        loss.backward ()
+        optimizer.step ()
 
     if (epoch + 1) % 100 == 0:
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+        print (f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item ():.4f}')
 
-print(f'final loss: {loss.item():.4f}')
+print (f'final loss: {loss.item ():.4f}')
 
 data = {
-    "model_state": model.state_dict(),
+    "model_state": model.state_dict (),
     "input_size": input_size,
     "output_size": output_size,
     "hidden_size": hidden_size,
@@ -111,6 +120,5 @@ data = {
 }
 
 FILE = "data.pth"
-torch.save(data, FILE)
-print(f'training complete. file saved to {FILE}')
-
+torch.save (data, FILE)
+print (f'training complete. file saved to {FILE}')
